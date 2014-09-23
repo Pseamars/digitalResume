679629433801ee2fa514c815b04bcc231b2e99cd
Pseamars <patrick.seamars@gmail.com>
2014-09-22 20:17:51 -0600

    Changes to background, and added badges

--- resume_2/resume_2_script.js
+++ resume_2/resume_2_script.js
@@ -1,12 +1,14 @@
 /* Hides and shows profile pic */
 
-$("#profile_pic").animate(
+$("#profile_pic").css("left", "-700px").animate(
 	{left: 0}, 
 	2000, 
 	"easeOutBounce", 
 	function  () {
 		console.log("I work!!");	
 	});
+
+
 	
  
 
