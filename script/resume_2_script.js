266fadcab39dd06fe995a6d85f26b4609df86a05
Pseamars <patrick.seamars@gmail.com>
2014-09-29 14:47:26 -0600

    Added quote and goal content

---
+++ script/resume_2_script.js
@@ -0,0 +1,20 @@
+/* Hides and shows profile pic */
+
+$("#profile_pic").css("left", "-700px").animate(
+	{left: 0}, 
+	2000, 
+	"easeOutBounce", 
+	function  () {
+		console.log("I work!!");	
+	});
+
+
+// $('.skillz_badge').hover(function () {
+// 	$(this).css(); 
+	
+// });
+ 
+
+
+
+
