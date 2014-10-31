082384377f28bb9e7aaf59868188dbdf3585bd04
Pseamars <patrick.seamars@gmail.com>
2014-10-31 10:58:00 -0600

    Added footer with dynamic time and copyright and some design stuff

--- script/resume_2_script.js
+++ script/resume_2_script.js
@@ -1,3 +1,9 @@
+
+// var minutes = setInterval(function  () {
+//     var today = new Date();
+//     var now = today.getMinutes(); 
+//     // return now;
+//     },1000);
 /* Moves profile pic */
 $("#profile_pic").css("left", "-700px").animate(
 	{left: 0}, 
@@ -7,19 +13,16 @@ $("#profile_pic").css("left", "-700px").animate(
 		console.log("I work!!");	
 	});
  
-// Hide Header on on scroll down
+// Hide Header on on scroll down - https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
 var didScroll; 
 var lastScrollTop = 0; 
-var delta = 5; 
-
-
 var headerHeight = $("header").outerHeight(); 
 
 $(window).scroll(function(event) {
     didScroll = true; 
 });
 
-setInterval(function(argument) {
+setInterval(function() {
     if(didScroll){
         hasScrolled(); 
         didScroll = false;
@@ -30,23 +33,24 @@ setInterval(function(argument) {
 function hasScrolled() {
     var st = $(this).scrollTop();
     
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    // Make sure they scroll more than the headerHeight
+    if(Math.abs(lastScrollTop - st) <= headerHeight)
         return;
     
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
+    // If they scrolled down and are past the navbar, hide the header
+    // ** This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > headerHeight){
         // Scroll Down
-        $('header').hide("slow"); //hide rather than fadeOut
+        $('header').add('#footer').hide(600); //hide rather than fadeOut because if fadeOut it takes away the element rather than just hiding it. It made the screen jumpy. The "slow" doesn't work with Safari
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
-            $('header').show("slow");
+            $('header').add('#footer').show(600);
         }
     }
     
     lastScrollTop = st;
+    console.log("I'm scrollin'");
 }
 // Fades goal background
 // $("#goalContainer").fadeIn(2000, function(){
@@ -70,3 +74,54 @@ $('#badge_container').on('click','button.skillz_badge',function(){
 
 // $('#description_container > div').hide()
 // $('#description_container > div[name=' + skill + ']').show()
+
+/*******************************************
+Footer
+********************************************/
+var today = new Date();
+
+//Copyright
+var year = today.getFullYear();
+var footDate = document.getElementById('copy'); 
+footDate.innerHTML = "Copyright &copy " + year;
+//Current time
+
+$(document).ready( function() {
+
+  function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridien = "AM"; 
+    if(hours > 12){
+        meridien = "PM"; 
+        hours = hours - 12;
+    }
+    if(minutes <= 10){
+        minutes = "0" + minutes;
+    }
+    if(hours === 0 ){
+        hours = 12;
+    }
+    // This gets a "handle" to the clock div in our HTML
+    var clockDiv = document.getElementById("time");
+
+    // Then we set the text inside the clock div 
+    // to the hours, minutes, and seconds of the current time
+    clockDiv.innerText = hours + ":" + minutes + " "+meridien;
+
+  }
+
+  // This runs the displayTime function the first time
+    displayTime();
+    setInterval(displayTime, 1000);
+});
+
+
+//var currentTime = hours+":"+minutes+" "+meridien;
+
+
+
+
+
